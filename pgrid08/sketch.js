a34b56d05182deeae4a80ebd8b45302917b9a521
mrsilvino <silvinomartins991@gmail.com>
2018-05-02 11:43:07 +0100

    updated css and minor sketch updates

--- pgrid08/sketch.js
+++ pgrid08/sketch.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+<< << << < HEAD
 var w = 600,
     h = 600;
 
@@ -25,51 +25,12 @@ function drawGrid() {
     multiply = 1;
     for (var x = spacing; x < width; x += spacing) {
         for (var y = spacing; y < height; y += spacing) {
-            for(var i=0;i<1+noise(x*y)*random(5);i++){
-                rect(x, y, spacing * 1/sqrt(i), spacing * 1/sqrt(i));
-                    
+            for (var i = 0; i < 1 + noise(x * y) * random(5); i++) {
+                rect(x, y, spacing * 1 / sqrt(i), spacing * 1 / sqrt(i));
+
             }
-           
-            
+
+
         }
     }
-
 }
-=======
-var w = 600,
-    h = 600;
-
-var spacing = 12;
-var multiply = 1;
-
-function setup() {
-    createCanvas(w, h);
-    rectMode(CENTER);
-    drawGrid();
-}
-
-function mousePressed() {
-    drawGrid();
-}
-
-function rand(max) {
-    return Math.round(Math.random() * (max - 1));
-}
-
-function drawGrid() {
-    clear();
-    background(0);
-    multiply = 1;
-    for (var x = spacing; x < width; x += spacing) {
-        for (var y = spacing; y < height; y += spacing) {
-            for(var i=0;i<1+noise(x*y)*random(5);i++){
-                rect(x, y, spacing * 1/sqrt(i), spacing * 1/sqrt(i));
-                    
-            }
-           
-            
-        }
-    }
-
-}
->>>>>>> 34fd8619bdf7148ea49a3f6dd1a1364a9c19cb7f
